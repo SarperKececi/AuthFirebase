6d3c4fcad0b9b4a745818d01d186b663bc29109e
GitHub Kullanıcı Adınız <GitHub E-Posta Adresiniz>
2023-12-23 21:08:38 +0300

    Initial Commit

---
+++ AuthFirebase/AuthFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthFirebaseApp.swift
+//  AuthFirebase
+//
+//  Created by Sarper Kececi on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AuthFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
